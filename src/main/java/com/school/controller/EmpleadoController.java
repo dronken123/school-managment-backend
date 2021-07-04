e3877e67f9fbdf10145ee3c35a9065e301546eb1
Anderson <anderson.bengolea@gmail.com>
2021-07-03 23:45:47 -0500

    implementación de especialidades

--- src/main/java/com/school/controller/EmpleadoController.java
+++ src/main/java/com/school/controller/EmpleadoController.java
@@ -3,7 +3,9 @@ package com.school.controller;
 import com.school.dao.EmpleadoDao;
 import com.school.model.Clase;
 import com.school.model.Empleado;
+import com.school.model.Especialidad;
 import com.school.service.EmpleadoService;
+import com.school.service.EspecialidadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
@@ -25,6 +27,9 @@ public class EmpleadoController {
     
     @Autowired
     private EmpleadoService empleadoService;
+
+    @Autowired
+    private EspecialidadService especialidadService;
 
     @GetMapping
     public ResponseEntity<List<Empleado>> getAllEmpleados(){
@@ -149,6 +154,7 @@ public class EmpleadoController {
             empleadoActual.setCorreo(empleado.getCorreo());
             empleadoActual.setSexo(empleado.getSexo());
             empleadoActual.setDomicilio(empleado.getDomicilio());
+            empleadoActual.setEspecialidades(empleado.getEspecialidades());
 
             empleadoActualizado = empleadoService.save(empleadoActual);
         } catch (DataAccessException e) {
@@ -177,6 +183,9 @@ public class EmpleadoController {
         }
 
         try {
+            for(Especialidad e: empleado.getEspecialidades()){
+                empleado.getEspecialidades().remove(e);
+            }
             empleadoService.delete(id);
         } catch (DataAccessException e) {
             response.put("mensaje", "Error al eliminar el empleado en la base de datos");
@@ -188,6 +197,43 @@ public class EmpleadoController {
         response.put("mensaje", "El empleado ha sido eliminado con éxito");
 
         return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/especialidades")
+    public ResponseEntity<List<Especialidad>> listarEspecialidades(){
+        return new ResponseEntity<List<Especialidad>>(especialidadService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/especialidades/crear")
+    public ResponseEntity<?> crearEspecialidad(@RequestBody Especialidad especialidad, BindingResult results){
+
+        Especialidad especialidadNueva = null;
+        Map<String, Object> response = new HashMap<>();
+
+        if(results.hasErrors()) {
+            List<String> errors = results.getFieldErrors()
+                    .stream()
+                    .map(er -> "El campo '" + er.getField() +"' " + er.getDefaultMessage())
+                    .collect(Collectors.toList());
+
+            response.put("errors", errors);
+
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            especialidadNueva = especialidadService.save(especialidad);
+        }catch (DataAccessException e) {
+            response.put("mensaje", "Error al insertar el empleado en la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "La especialidad se creo con éxito!");
+        response.put("especialidad", especialidadNueva);
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
     
 }
